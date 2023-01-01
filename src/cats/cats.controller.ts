d3dd58d3f25496b8612acf5e7fb3a4ee0a7510ff
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-01 18:21:00 +0900

    20230101

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    @Get()
+    hello(@Req() request: Request): Request {
+        return request;
+    }
+
+    @Get('goodbye')
+    goodbye(): String {
+        return "goodbye!";
+    }
+}
