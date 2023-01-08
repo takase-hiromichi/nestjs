fb61d1061c88b97a3eede3ef42e75241e44207b0
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-08 20:16:00 +0900

    20230108

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
 import { CustomException } from 'src/exception/custom.exception';
+import { HttpFilter } from '../filter/http/http.filter';
 
 @Controller('cats')
 export class CatsController {
     @Get()
+    @UseFilters(new HttpFilter())
     findAll() {
         throw new CustomException();
     }
