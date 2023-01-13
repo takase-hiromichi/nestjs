faeaf40a99aea0cced7565eaacbd800564ba5656
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-13 21:10:32 +0900

    20230113-1

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateCatDto } from 'src/dto/cat/cat.dto';
-import { Cat } from './cats.interface';
-import { CatsService } from './cats.service';
-import { ValidationPipe } from '../pipe/validation/validation.pipe';
-import { Roles } from 'src/decorator/roles/roles.decorator';
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { CreateCatDto } from "src/dto/cat/cat.dto";
+import { Cat } from "./cats.interface";
+import { CatsService } from "./cats.service";
+import { ValidationPipe } from "../pipe/validation/validation.pipe";
+import { Roles } from "src/decorator/roles/roles.decorator";
+import { RolesGuard } from "src/guard/roles/roles.guard";
 
-@Controller('cats')
+@Controller("cats")
+@UseGuards(RolesGuard)
 export class CatsController {
-    constructor(private catsService: CatsService) {}
-    
-    @Post()
-    @Roles('admin')
-    create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
-        this.catsService.create(createCatDto);
-    }
-    @Get()
-    findAll(): Cat[] {
-        return this.catsService.findAll();
-    }
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  @Roles("admin")
+  create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+  @Get()
+  findAll(): Cat[] {
+    return this.catsService.findAll();
+  }
 }
