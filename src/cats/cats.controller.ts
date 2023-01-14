98e336358ad9897a84fc3541139368a8a0c8ee19
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-14 21:29:22 +0900

    20230114

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,18 +1,16 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseInterceptors } from "@nestjs/common";
 import { CreateCatDto } from "src/dto/cat/cat.dto";
 import { Cat } from "./cats.interface";
 import { CatsService } from "./cats.service";
 import { ValidationPipe } from "../pipe/validation/validation.pipe";
-import { Roles } from "src/decorator/roles/roles.decorator";
-import { RolesGuard } from "src/guard/roles/roles.guard";
+import { LoggingInterceptor } from "src/interceptor/logging/logging.interceptor";
 
 @Controller("cats")
-@UseGuards(RolesGuard)
+@UseInterceptors(LoggingInterceptor)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @Roles("admin")
   create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
---
+++ src/interceptor/logging/logging.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log("Before...");
+
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        console.log(`after... ${Date.now() - now}ms`);
+      })
+    );
+  }
+}
