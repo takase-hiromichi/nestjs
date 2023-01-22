2ed127adf1ecfb0acd1a35d315b2d2e78ae42c6b
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-22 21:41:19 +0900

    20230122

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,33 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsModule } from './cats/cats.module';
-import { logger } from './middleware/logger/logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CatsController } from "./cats/cats.controller";
+import { CatsModule } from "./cats/cats.module";
+import { logger } from "./middleware/logger/logger.middleware";
+import { User } from "./user/entities/user.entity";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "root",
+      database: "test",
+      entities: [User],
+      synchronize: true,
+    }),
+    CatsModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(logger)
-      .forRoutes(CatsController);
+    consumer.apply(logger).forRoutes(CatsController);
   }
 }
