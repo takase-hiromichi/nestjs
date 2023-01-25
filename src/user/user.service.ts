80993cd0f824b3058d8832ace3a1ce3eae0425e7
takase hiromichi <takase.hiromichi.ex@gmail.com>
2023-01-25 20:52:49 +0900

    20230125

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DeleteResult, Repository } from "typeorm";
+import { DataSource, DeleteResult, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
@@ -8,11 +8,28 @@ import { User } from "./entities/user.entity";
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private dataSource: DataSource
   ) {}
 
   create(createUserDto: CreateUserDto) {
     return "This action adds a new user";
+  }
+
+  async createMany(users: User[]) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.save(users[0]);
+      await queryRunner.manager.save(users[1]);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   findAll(): Promise<User[]> {
